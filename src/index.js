1d581fdbc95138dc4aae64d98bcf6494e70f06f4
lihaichen <li8303@yeah.net>
2017-05-12 19:09:28 +0800

    增加调试功能

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './routes';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './redux/reducer';
 import createMiddleware from './redux/middleware/clientMiddleware';
 import ApiClient from './utils/api_client';
+
+
 const client = new ApiClient();
-const store = createStore(reducer, applyMiddleware(createMiddleware(client)));
+const enhancer = compose(
+  applyMiddleware(createMiddleware(client)),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+const store = createStore(reducer, enhancer);
 ReactDOM.render(
   (<Provider store={store}>
     {routes}
